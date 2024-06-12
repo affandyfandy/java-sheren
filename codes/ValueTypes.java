a7e102195891199d9c11af9be9210d7a7c146889
sheren <sherenyang07@gmail.com>
2024-06-12 11:29:23 +0700

    update valuetypes code

--- codes/ValueTypes.java
+++ codes/ValueTypes.java
@@ -1,3 +1,10 @@
+class PassingValue {
+    public void example(int x, int y) {
+        x++;
+        y++;
+    }
+}
+
 public class ValueTypes {
     public static void main(String[] args) {
         int a = 5;
@@ -6,6 +13,7 @@ public class ValueTypes {
         System.out.println(b); // output: 5
         System.out.println(a); // output: 15
 
+
         // Passed to a method example:
         int num = 10;
         System.out.println("Before calling method: " + num); // output: 10
@@ -13,11 +21,23 @@ public class ValueTypes {
         modifyValue(num);
 
         System.out.println("After calling method: " + num); // output: 10
+
+
+        //Passed as parameters into function example:
+        int x = 5;
+        int y = 10;
+ 
+        PassingValue object = new PassingValue();
+ 
+        System.out.println("Value of x = " + x + " and y = " + y);
+ 
+        object.example(x, y);
+
+        System.out.println("Value of x = " + x + " and y = " + y);
     }
 
     public static void modifyValue(int x) {
         x = 100;
         System.out.println("Inside method: " + x); // output: 100
     }
-
 }
