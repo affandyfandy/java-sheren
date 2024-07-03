da76d8cb82f5ea1e5137ab76dfb46787c53fc25a
sheren <sherenyang07@gmail.com>
2024-07-03 10:24:56 +0700

    Feat: Add Assignment 2 Q3

---
+++ Week 04/Lecture 07/Assignment 2/Q3/assignment2q3/src/test/java/com/example/assignment2q3/Assignment2q3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.assignment2q3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Assignment2q3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
